49387d09e14346ac658c196112d269a54c02d5da
Abdullah Al Naiem <mail@naiem.info>
2024-10-09 02:44:54 +0600

    Data restore from Trash/SoftDeletes

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\MediaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+// Student
 Route::post('/student', [StudentController::class, 'store']);
 Route::get('/student', [StudentController::class, 'index']);
 Route::put('/student', [StudentController::class, 'update']);
 Route::delete('/student', [StudentController::class, 'destroy']);
+Route::patch('/student', [StudentController::class, 'restore']);
